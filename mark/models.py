512953615150a5fe2a62cacaf070bf47447e4a98
solone313 <solone313@naver.com>
2020-01-10 03:05:02 +0900

    add url&views&list.html

--- mark/models.py
+++ mark/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=200)
     url = models.URLField('Site URL')
+    def __str__(self):
+        return "이름 : " + self.site_name + ", 주소 : " + self.url
