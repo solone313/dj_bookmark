a15e6e1bfe109f4b059dae4ba9d7b8bd204a063e
solone313 <solone313@naver.com>
2020-01-10 03:23:30 +0900

    add createView

--- mark/urls.py
+++ mark/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
 ]
