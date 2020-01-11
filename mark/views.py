c21cfec4d62e67c7057f367a424f958b2dc0d692
solone313 <solone313@naver.com>
2020-01-11 18:06:30 +0900

    add detailView

--- mark/views.py
+++ mark/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Bookmark
 # Create your views here.
@@ -13,3 +14,6 @@ class BookmarkCreateView(CreateView):
     fields = ['site_name', 'url']
     success_url = reverse_lazy('list')
     template_name_suffix = '_create'
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
